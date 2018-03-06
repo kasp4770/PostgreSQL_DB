a8546c95680d508202c1328752474ae3a085b7d6
Michael Hansen <michaelhansenit@live.dk>
2018-03-06 13:20:55 +0100

    PostgreSQL Database connector - initial commit

---
+++ src/Connection_Test.java
@@ -0,0 +1,7 @@
+import java.sql.*;
+
+public class Connection_Test {
+    public static void main(String[] args) {
+        Connection conn = DB_Connector.connect();
+    }
+}
---
+++ src/DB_Connector.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class DB_Connector {
+
+    // PostgreSQL JDBC Driver
+    private static String url = "jdbc:postgresql://localhost:5432/MyDatabase";
+    // user name
+    private static String user = "postgres";
+    // pass word
+    private static String pass = "030493";
+    private static Connection conn = null;
+
+    public static Connection connect(){
+        System.out.println("\n--Connecting to postgreSQL JDBC--");
+        // Locate PostgreSQL JDBC Driver
+        try {
+            conn = DriverManager.getConnection(url, user, pass);
+        }
+        // Catch exceptions if JDBC is not found
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        // if connection successful
+        if (conn != null) {
+            System.out.println("\n--It worked! Connection successful--");
+        } else {
+            System.out.println("\n--Connection failed--");
+        }
+        return conn;
+
+    }
+}
